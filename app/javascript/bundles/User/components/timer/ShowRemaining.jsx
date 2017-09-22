494015155e1ba42eb2a8133ed429772c767d7b8a
fujisawakyle <fujisawakyle@gmail.com>
2017-09-21 18:22:44 -0700

    fix ShowRemaining format

--- app/javascript/bundles/User/components/timer/ShowRemaining.jsx
+++ app/javascript/bundles/User/components/timer/ShowRemaining.jsx
@@ -8,13 +8,19 @@ const style = {
 export default class ShowRemaining extends Component {
     constructor(props) {
         super(props);
-
     }
+
     render () {
+        let showRemaining;
+            if (this.props.hours === 0) {
+                    showRemaining = `${this.props.minutes}:${this.props.seconds}`
+                }
+            else {
+                    showRemaining = `${this.props.hours}:${this.props.minutes}:${this.props.seconds}`
+                }
         return (
             <div className="timer__remaining" style={style.showTime} >
-                {this.props.hours}:{this.props.minutes}:{this.props.seconds} <br />
-                nextsave: {this.props.logTime}
+                {showRemaining} <br />
             </div>
         )
     }
