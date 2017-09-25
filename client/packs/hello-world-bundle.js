4383f6f9aca8fa4267d94f2d3abdceba1d40e877
fujisawakyle <fujisawakyle@gmail.com>
2017-09-24 18:04:05 -0700

    delete helloworld files

--- client/packs/hello-world-bundle.js
+++
@@ -1,9 +0,0 @@
-import ReactOnRails from 'react-on-rails';
-
-import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
-import User from '../bundles/User/components/User';
-
-// This is how react_on_rails can see the HelloWorld in the browser.
-ReactOnRails.register({
-  HelloWorld
-});
