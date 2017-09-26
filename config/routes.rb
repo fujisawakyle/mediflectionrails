c9f46d8ad065796ee41437bd2262622bb81dd640
fujisawakyle <fujisawakyle@gmail.com>
2017-09-25 17:26:49 -0700

    change user relationship

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  #accept data back from FB
-  # devise_for :users
-  namespace :api do
-    # resources :users
-    resources :mediflections
-  end
-  # namespace :api do
-  #   namespace :v1 do
-  #     resources :items, only: [:index, :create, :destroy, :update]
-  #   end
-  # end
 
   get 'static_pages/home'
 
   root 'static_pages#home'
-  # get 'users/new'
+
   get 'mediflections', to: 'mediflections#index'
-  # get 'hello_world', to: 'hello_world#index'
-  # get 'signup', to: 'users#new'
-  # post 'signup', to: 'users#create'
-  # resources :users
+
   resources :mediflections,        only: [:create, :update]
 end
--- db/migrate/20170914153712_devise_create_users.rb
+++ db/migrate/20170914153712_devise_create_users.rb
@@ -1,7 +1,6 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.has_many :mediflections
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
